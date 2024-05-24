b3f40421950128552daf8c3abdc95afb72a4ac3c
Moritz Weckbecker <moritz.weckbecker@hhi.fraunhofer.de>
2024-05-24 16:18:08 +0200

    Retraining metrics

--- src/metrics/retrain_metrics.py
+++ src/metrics/retrain_metrics.py
@@ -1,8 +1,10 @@
 from utils import Metric
 import torch
+from torch.utils.data import DataLoader
 import copy
 from utils.data import RestrictedDataset
-from train import start_training
+from train import load_loss, load_optimizer, load_scheduler, load_augmentation
+from tqdm import tqdm
 
 class RetrainMetric(Metric):
     name = "RetrainMetric"
@@ -10,60 +12,245 @@ class RetrainMetric(Metric):
     def __init__(self, train, test, model, device="cuda"):
         self.train = train
         self.test = test
+        self.model = model #load model WITHOUT checkpoint in evaluate script for this metric!
+        self.device = device
 
+    def load_retraining_parameters(self): #only for know, this will need to be loaded
+        self.num_classes = 10
+        self.epochs = 5
+        self.batch_size = 64
+        self.lr = 0.005
+        self.augmentation = None
+        self.optimizer = None
+        self.scheduler = None
+        self.loss = None
 
+    def retrain(self, ds, num_classes, epochs, batch_size, lr,
+                augmentation, optimizer, scheduler, loss):
+        #tensorboarddir = f"{model_name}_{lr}_{scheduler}_{optimizer}{f'_aug' if augmentation is not None else ''}"
+        #tensorboarddir = os.path.join(save_dir, tensorboarddir)
+        #writer = SummaryWriter(tensorboarddir)
+        model = copy.deepcopy(self.model)
 
-    def retrain(self, indices):
-        #
-        start_training(
-            model_name=train_config.get('model_name', None),
-            model_path=train_config.get('model_path', None),
-            base_epoch=train_config.get('base_epoch', 0),
-            device=self.device,
-            num_classes=train_config.get('num_classes', None),
-            class_groups=train_config.get('class_groups', None),
-            dataset_name=train_config.get('dataset_name', None),
-            dataset_type=train_config.get('dataset_type', 'std'),
-            data_root=train_config.get('data_root', None),
-            epochs=train_config.get('epochs', None),
-            batch_size=train_config.get('batch_size', None),
-            lr=train_config.get('lr', 0.1),
-            augmentation=train_config.get('augmentation', None),
-            loss=train_config.get('loss', None),
-            optimizer=train_config.get('optimizer', None),
-            save_dir=train_config.get('save_dir', None),
-            save_each=train_config.get('save_each', 100),
-            num_batches_eval=train_config.get('num_batches_eval', None),
-            validation_size=train_config.get('validation_size', 2000),
-            scheduler=train_config.get('scheduler', None),
-            train_indices=indices
-            )
-        return retrained_model
+        learning_rates=[]
+        train_losses = []
+        #validation_losses = []
+        #validation_epochs = []
+        #val_acc = []
+        train_acc = []
+        loss=load_loss()
+        optimizer = load_optimizer(optimizer, model, lr)
+        scheduler = load_scheduler(scheduler, optimizer)
+        if augmentation is not None:
+            augmentation = load_augmentation(augmentation)
+
+        #kwargs = {
+        #    'data_root': data_root,
+        #    'class_groups': class_groups,
+        #    'image_set': "val",
+        #    'validation_size': validation_size,
+        #    'only_train': True
+        #}
+            
+        #corrupt = (dataset_type == "corrupt")
+        #group = (dataset_type == "group")
+        loader = DataLoader(ds, batch_size=batch_size, shuffle=True)
+        #saved_files = []
+
+        #if model_path is not None:
+        #    checkpoint = torch.load(model_path, map_location=self.device)
+        #    model.load_state_dict(checkpoint["model_state"])
+        #    optimizer.load_state_dict(checkpoint["optimizer_state"])
+        #    scheduler.load_state_dict(checkpoint["scheduler_state"])
+        #    train_losses = checkpoint["train_losses"]
+        #    validation_losses = checkpoint["validation_losses"]
+        #    validation_epochs = checkpoint["validation_epochs"]
+        #    val_acc = checkpoint["validation_accuracy"]
+        #    train_acc = checkpoint["train_accuracy"]
+
+        #for i,r in enumerate(learning_rates):
+        #    writer.add_scalar('Metric/lr', r, i)
+        #for i, r in enumerate(train_acc):
+        #    writer.add_scalar('Metric/train_acc', r, i)
+        #for i, r in enumerate(val_acc):
+        #    writer.add_scalar('Metric/val_acc', r, validation_epochs[i])
+        #for i, l in enumerate(train_losses):
+        #    writer.add_scalar('Loss/train', l, i)
+        #for i, l in enumerate(validation_losses):
+        #    writer.add_scalar('Loss/val', l, validation_epochs[i])
+
+        model.train()
+        #best_model_yet = model_path
+        #best_loss_yet = None
+
+        #if not os.path.isdir(save_dir):
+        #    os.makedirs(save_dir,exist_ok=True)
+        for e in range(epochs):
+            y_true = torch.empty(0, device=self.device)
+            y_out = torch.empty((0, num_classes), device=self.device)
+            cum_loss = 0
+            cnt = 0
+            for inputs, targets in tqdm(iter(loader)):
+                inputs = inputs.to(self.device)
+                targets = targets.to(self.device)
+            
+                if augmentation is not None:
+                    inputs=augmentation(inputs)
+
+                y_true = torch.cat((y_true, targets), 0)
+
+                optimizer.zero_grad()
+                logits = model(inputs)
+                l = loss(logits, targets)
+                y_out = torch.cat((y_out, logits.detach().clone()), 0)
+                ''' comment out broken model check
+                if math.isnan(l):
+                    if not os.path.isdir("./broken_model"):
+                        os.mkdir("broken_model")
+                    save_dict = {
+                        'model_state': model.state_dict(),
+                        'optimizer_state': optimizer.state_dict(),
+                        'scheduler_state': scheduler.state_dict(),
+                        'epoch': base_epoch + e,
+                        'learning_rates': learning_rates,
+                        'train_losses': train_losses,
+                        'validation_losses': validation_losses,
+                        'validation_epochs': validation_epochs,
+                        'validation_accuracy': val_acc,
+                        'ds_type':dataset_type,
+                        'train_accuracy': train_acc
+                    }
+                    path = os.path.join("./broken_model", f"{dataset_name}_{model_name}_{base_epoch + e}")
+                    torch.save(save_dict, path)
+                    print("NaN loss")
+                    exit()
+                '''
+                l.backward()
+                optimizer.step()
+                cum_loss = cum_loss + l
+                cnt = cnt + inputs.shape[0]
+            #y_out = torch.softmax(y_out, dim=1)
+            y_pred = torch.argmax(y_out, dim=1)
+            # y_true = y_true.cpu().numpy()
+            # y_out = y_out.cpu().numpy()
+            # y_pred = y_pred.cpu().numpy()
+            train_loss = cum_loss.detach().cpu()
+            acc = (y_true == y_pred).sum() / y_out.shape[0]
+            train_acc.append(acc)
+            print(f"train accuracy: {acc}")
+            #writer.add_scalar('Metric/train_acc', acc, base_epoch + e)
+            #writer.add_scalar('Metric/learning_rates', 0.95, base_epoch + e)
+            train_losses.append(train_loss)
+            #writer.add_scalar('Loss/train', train_loss, base_epoch + e)
+            print(f"Epoch {e + 1}/{epochs} loss: {cum_loss}")  # / cnt}")
+            print("\n==============\n")
+            learning_rates.append(scheduler.get_lr())
+            scheduler.step()
+            '''
+            if (e + 1) % save_each == 0:
+                validation_loss = get_validation_loss(model, valds, loss, device)
+                validation_losses.append(validation_loss.detach().cpu())
+                validation_epochs.append(e)
+                save_dict = {
+                    'model_state': model.state_dict(),
+                    'optimizer_state': optimizer.state_dict(),
+                    'scheduler_state': scheduler.state_dict(),
+                    'epoch': base_epoch + e,
+                    'train_losses': train_losses,
+                    'validation_losses': validation_losses,
+                    'validation_epochs': validation_epochs,
+                    'validation_accuracy': val_acc,
+                    'train_accuracy': train_acc
+                }
+                if corrupt:
+                    save_dict["corrupt_samples"] = ds.dataset.corrupt_samples
+                    save_dict["corrupt_labels"] = ds.dataset.corrupt_labels
+                if group:
+                    save_dict["classes"] = ds.dataset.classes
+                save_id = f"{dataset_name}_{model_name}_{base_epoch + e}"
+                path = os.path.join(save_dir, save_id)
+                if not os.path.isdir(save_dir):
+                    os.makedirs(save_dir, exist_ok=True)
+                torch.save(save_dict, path)
+                saved_files.append((path, save_id))
+
+                print(f"\n\nValidation loss: {validation_loss}\n\n")
+                #writer.add_scalar('Loss/val', validation_loss, base_epoch + e)
+                valeval = evaluate_model(model_name=model_name, device=device, num_classes=num_classes,
+                                        data_root=data_root,
+                                        batch_size=batch_size, num_batches_to_process=num_batches_eval,
+                                        load_path=best_model_yet, dataset_name=dataset_name, dataset_type=dataset_type,
+                                        validation_size=validation_size,
+                                        image_set="val", class_groups=class_groups
+                                        )
+                print(f"validation accuracy: {valeval}")
+                #writer.add_scalar('Metric/val_acc', valeval, base_epoch + e)
+                val_acc.append(valeval)
+                if best_loss_yet is None or best_loss_yet > validation_loss:
+                    best_loss_yet = validation_loss
+                    path = os.path.join(save_dir, f"best_val_score_{dataset_name}_{model_name}_{base_epoch + e}")
+                    torch.save(save_dict, path)
+                    if best_model_yet is not None:
+                        os.remove(best_model_yet)
+                    best_model_yet = path
+                '''    
+            #writer.flush()
+        #writer.close()
+        #save_id = os.path.basename(best_model_yet)
+        return model
     
-# Parent Retrain Metric should only have method 'retrain' as scores etc are all different depending on the metric
+    def evaluate(self, retrained_model, evalds, batch_size, num_classes, num_batches_to_process):
+        retrained_model.eval()
+        loader = DataLoader(evalds, batch_size=batch_size, shuffle=True)
+        y_true = torch.empty(0, device=self.device)
+        y_out = torch.empty((0, num_classes), device=self.device)
+
+        for i, (inputs, targets) in enumerate(tqdm(iter(loader), total=min(num_batches_to_process, len(loader)))):
+            inputs = inputs.to(self.device)
+            targets = targets.to(self.device)
+            y_true = torch.cat((y_true, targets), 0)
+            with torch.no_grad():
+                logits = retrained_model(inputs)
+            y_out = torch.cat((y_out, logits), 0)
+
+        y_out = torch.softmax(y_out, dim=1)
+        y_pred = torch.argmax(y_out, dim=1)
+        return (y_true == y_pred).sum() / y_out.shape[0]
+    
+
     
 class LeaveBatchOut(RetrainMetric):
     name = "LeaveBatchOutMetric"
 
-    def __init__(self, train, test, model, batch_nr=10, device="cuda"):
+    def __init__(self, train, test, num_classes, model, batch_nr=10, device="cuda"):
         self.train = train
         self.test = test
+        self.num_classes = num_classes
         self.model = model
-        self.scores = torch.empty(0, dtype=torch.float, device=device)
+        self.scores = torch.empty(batch_nr, dtype=torch.float, device=device)
         self.batch_nr = batch_nr
         self.batchsize = len(self.train) // batch_nr
         self.device = device
+        self.load_retraining_parameters()
 
     def __call__(self, xpl):
         xpl.to(self.device)
         combined_xpl = xpl.sum(dim=0)
         indices_sorted = combined_xpl.argsort(descending=True)
         for i in range(self.batch_nr):
-            # maybe write a load_restricted_dataset with indices instead and call it in start_training?
             ds = RestrictedDataset(self.train, indices_sorted[:(i+1)*self.batchsize], return_indices=True)
             evalds = self.test
-            # retrain
-            # correct accuracy on evalds
+            retrained_model = self.retrain(ds, )
+            eval_accuracy = self.evaluate(retrained_model, evalds, batch_size=self.batchsize, num_classes=self.num_classes, num_batches_to_process=self.batch_nr)
+            self.scores[i] = eval_accuracy
+
+    def get_result(self, dir=None, file_name=None):
+        self.scores = self.scores.to('cpu').detach().numpy()
+        resdict = {'metric': self.name, 'all_scores': self.scores, 'score_for_most_relevant_batch': self.scores[0],
+                   'num_batches': self.scores.shape[0]}
+        if dir is not None:
+            self.write_result(resdict, dir, file_name)
+        return resdict
 
 '''
         # copy model with blank weights
